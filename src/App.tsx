afe2eccd1fe951d45359f5bb8825ca0bf92e6e5c
xdtr27 <alexandreconta617@gmail.com>
2024-08-31 23:49:33 -0300

    tratamento de erros

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { useEffect, useState } from "react";
+import Toastify from "toastify-js";
 import "./App.module.css";
+
 import { CirclePlus } from "lucide-react";
 import { Header } from "./components/Header";
 import styles from "./App.module.css";
 import Input from "./components/Input";
 import TaskCard from "./components/TaskCard";
-import { useEffect, useState } from "react";
 
 type Task = {
   id: number;
@@ -14,7 +16,7 @@ type Task = {
 
 function App() {
   const data = localStorage.getItem("taskList")
-    ? JSON.parse(localStorage.getItem("taskList")!) // ! added to assert that the value is not null
+    ? JSON.parse(localStorage.getItem("taskList")!)
     : [];
 
   const [tasks, setTask] = useState(data);
@@ -24,11 +26,31 @@ function App() {
     const inputContent = document.getElementById(
       "central-input"
     ) as HTMLInputElement;
-    setTask([
-      ...tasks,
-      { id: tasks.length + 1, content: inputContent.value, checked: false },
-    ]);
-    inputContent.value = ""; // clear input field after task is added
+    try {
+      console.log(inputContent.value);
+      if (inputContent.value) {
+        setTask([
+          ...tasks,
+          { id: tasks.length + 1, content: inputContent.value, checked: false },
+        ]);
+      } else {
+        throw new Error("Você precisa adicionar uma descrição!");
+      }
+    } catch (error) {
+      console.error(error);
+      if (error instanceof Error) {
+        Toastify({
+          text: error.message,
+          position: "center",
+          duration: 4000,
+          style: {
+            background: "#e25858",
+          },
+          close: true,
+        }).showToast();
+      }
+    }
+    inputContent.value = "";
   }
 
   useEffect(() => {
